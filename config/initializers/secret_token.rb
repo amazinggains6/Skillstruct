373489a855ce7bf99b4a2236bd6241ffd2a5fb78
Zach Sherman <zksherm@gmail.com>
2011-12-11 23:02:41 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subscripty::Application.config.secret_token = '4021dcd5b883696270a14efe51c9a4676cdaa9cb11eb37e7566ad52b117cf481fba8b9ad847b9985a3029033ff8e787018ef06a793db66fd9c20a45d4786c1af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Subscripty::Application.config.session_store :cookie_store, key: '_subscripty_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Subscripty::Application.config.session_store :active_record_store
