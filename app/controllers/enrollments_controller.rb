e0ab9b216258eaa81c63432d15e4427d1fa14457
Zach Sherman <zksherm@gmail.com>
2011-12-27 21:07:14 -0600

    Fixed attendance limit

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,11 +2,15 @@ class EnrollmentsController < ApplicationController
   
   def create
     @course = Course.find(params[:enrollment][:course_id])
-    @enrollments = @course.enrollments.count  
-    current_user.enroll!(@course, current_user)
-    flash[:notice] = "Congratulations, your enrollment is complete."
-    redirect_to @course
-    
+    enrollments = @course.enrollments.count  
+      if enrollments < @course.attendance
+        current_user.enroll!(@course, current_user)
+        flash[:notice] = "Congratulations, your enrollment is complete."
+        redirect_to @course
+      else
+        flash[:notice] = "Sorry, this class is full."
+        redirect_to @course
+      end
   end
   
   def destroy
