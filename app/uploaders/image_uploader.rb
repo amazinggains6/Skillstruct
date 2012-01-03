0abd67ef972a8528d06f2a6817726d5e27023b9a
Zach Sherman <zksherm@gmail.com>
2012-01-02 20:20:08 -0600

    Added some course partial CSS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_limit => [50, 50]
+     process :resize_to_limit => [60, 60]
   end
   
   version :show do
