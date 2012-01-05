a4c1b92a792b2e8f3040f87772fc8313ba067218
Zach Sherman <zksherm@gmail.com>
2012-01-04 22:01:06 -0600

    Added active mailer settings to work with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,12 @@ Subscripty::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.default_url_options = { :host => 'empty-robot-8386.herokuapp.com' }
-  
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
