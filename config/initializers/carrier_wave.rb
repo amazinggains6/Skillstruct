0e9d145733bc7c9ca976eb12599d87646529f508
Zach Sherman <zksherm@gmail.com>
2012-01-08 00:29:51 -0600

    Adding Amazon credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => 'Osfg8ySrsPtyfji40HUW48nzUebGs0+XWFccV+bL',       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'zach-rails-app'                     # required
-  config.fog_host       = 'http://#{config.fog_directory}.s3.amazonaws.com'            # optional, defaults to nil
+  config.fog_directory  = 'zach_rails_app'                     # required
+  config.fog_host       = 'https://zach_rails_app.s3.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
