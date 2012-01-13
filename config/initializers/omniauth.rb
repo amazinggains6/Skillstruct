1ea9a57965d62059cd3833e56fcc3b8ade42cc96
Zach Sherman <zksherm@gmail.com>
2012-01-13 01:35:49 -0600

    Fixed development env error with Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :twitter, "eq004itbu3iNPUECSfgfjg", "VqmbAxwPslXY2xq4uTe0SrGp3CW7xrkGjNeeKjdtZw8"
 end
