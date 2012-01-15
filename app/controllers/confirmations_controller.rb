8536047e227252e15fcb744dc17cf1cfb4d0c748
Zach Sherman <zksherm@gmail.com>
2012-01-14 21:55:46 -0600

    Fixing confirmation show

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,12 +2,10 @@ class ConfirmationsController < Devise::ConfirmationsController
   
   def show
     super
-    if @user.facebook
       @user.facebook.feed!(
         :message => 'I just signed up for Skillstruct.com', 
         :name => 'Skillstruct'
       )
-    elsif @user.twitter
       @user.twitter.update("My Rails 3 App with Omniauth, Devise and Twitter")  
     else
       
