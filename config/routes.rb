dde8b4eb8f3d6aa6c25dbf0bbf5c2aeacbf7c07c
Zach Sherman <zksherm@gmail.com>
2012-01-14 20:41:34 -0600

    Added custom controller for confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Subscripty::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => 'registrations'}
+  devise_for :users, :controllers => {:registrations => 'registrations', :confirmations => 'confirmations'}
   resources :users
   resources :courses do
     resources :purchases
