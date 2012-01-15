0cd5b5e1699fa8e6794ff27729668895bad4dd42
Zach Sherman <zksherm@gmail.com>
2012-01-14 22:34:26 -0600

    Fixing authentication posting

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,10 +2,17 @@ class ConfirmationsController < Devise::ConfirmationsController
   
   def show
     super
-      @user.facebook.feed!(
-        :message => 'I just signed up for Skillstruct.com', 
-        :name => 'Skillstruct'
-      )
-      @user.twitter.update("My Rails 3 App with Omniauth, Devise and Twitter")  
+    @authentications = @user.authentications
+    @authentications.each do |authentication|
+      if authentication.provider == "facebook"
+        @user.facebook.feed!(
+          :message => 'I just signed up for Skillstruct.com', 
+          :name => 'Skillstruct'
+          )
+      elsif authentication.provider == "twitter"
+        @user.twitter.update("My Rails 3 App with Omniauth, Devise and Twitter")
+      else
+    end 
+   end 
   end
 end
